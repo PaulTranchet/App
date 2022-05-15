e699e34e328d33e4ee06c3a7a4e2ccb5e2f19a70
PC_ <paul.tranchet@efrei.net>
2022-05-15 15:52:18 +0200

    base project working

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -20,14 +20,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             fitsSystemWindows = false
         )
 
-
         setSupportActionBar(findViewById<Toolbar>(R.id.toolbar)?.apply {
             doOnApplyWindowInsets { systemBarsInsets ->
                 (layoutParams as ViewGroup.MarginLayoutParams).topMargin = systemBarsInsets.top
             }
             title = ""
         })
-
 
         supportFragmentManager.commit {
             add(R.id.containerFragment, MainFragment::class.java, Bundle())
--- app/src/main/java/com/example/app/MainFragment.kt
+++ app/src/main/java/com/example/app/MainFragment.kt
@@ -1,39 +1,64 @@
 package com.example.app
 
+import android.graphics.Bitmap
+import android.media.Image
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
-import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.google.android.filament.utils.HDRLoader
+import com.google.ar.core.Frame
 import dev.romainguy.kotlin.math.Float3
 import io.github.sceneview.SceneView
-import io.github.sceneview.environment.loadEnvironment
+import io.github.sceneview.ar.ArSceneView
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 typealias Position = Float3
 typealias Rotation = Float3
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    lateinit var sceneView: SceneView
-    lateinit var loadingView: View
+    lateinit var sceneView: ArSceneView
 
+    var capturePicture : Boolean = false
+
+    fun setCapturePictureToTrue()
+    {
+        this.capturePicture = true;
+    }
+
+    fun takePhoto()
+    {
+        if (capturePicture == true) {
+            try {
+                val root: File? = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                val frame: Frame = sceneView.arSession!!.update()
+                var image: Image = frame.acquireCameraImage()
+                val h = image.height
+                val w = image.width
+                var bitmap: Bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+
+                val filename: String = root.toString() + File.separator + "test.jpeg"
+                Log.e("TAKE PHOTO", filename)
+                FileOutputStream(filename).use { out: OutputStream ->
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+                }
+            } catch (t: Exception) {
+                Log.e("TAKE PHOTO", "Exception on the OpenGL thread", t);
+            }
+            capturePicture = false
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         sceneView = view.findViewById(R.id.sceneView)
-        loadingView = view.findViewById(R.id.loadingView)
-
-        sceneView.camera.position = Position(x = 4.0f, y = -1.0f)
-        sceneView.camera.rotation = Rotation(x = 0.0f, y = 80.0f)
-
-        lifecycleScope.launchWhenCreated {
-            sceneView.camera.smooth(
-                position = Position(x = -1.0f, y = 1.5f, z = -3.5f),
-                rotation = Rotation(x = -60.0f, y = -50.0f),
-                speed = 0.5f
-            )
+        sceneView.onArFrame = {
+            takePhoto();
         }
     }
 }
